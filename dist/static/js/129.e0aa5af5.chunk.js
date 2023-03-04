7fc8e02746e36bd144907ec00c4f728775805922
goodCj <cj950218@163.com>
2023-03-04 14:26:34 +0800

    优化压缩

--- dist/static/js/129.e0aa5af5.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgame=self.webpackChunkgame||[]).push([[129],{129:function(e,a,c){c.r(a);var s=c(3494),p=c.n(s),f=c(184);a.default=function(){return(0,f.jsx)("div",{className:"pdf",children:(0,f.jsx)(p(),{className:"pdfBox",fileType:"docx",filePath:"http://hpip.work/docx/privacy.docx"})})}}}]);
