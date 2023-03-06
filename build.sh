9785440d20e7b88e4e6ca2460dc3f6340ce6b504
goodCj <cj950218@163.com>
2023-03-06 22:43:38 +0800

    修改commit

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf game_web
 
 git clone https://github.com/goodCj/game_web.git
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/local/nginx/html/*
 
-cp -r ./game_web/dist/* /usr/share/nginx/html/
+cp -r ./game_web/dist/* /usr/local/nginx/html/
 
