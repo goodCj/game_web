3c2a8068a8cebb164fd50b13a8682e10ccb46109
goodCj <cj950218@163.com>
2023-03-07 23:04:24 +0800

    2222

--- src/pages/pdf/privacy.jsx
+++ src/pages/pdf/privacy.jsx
@@ -1,20 +1,13 @@
-import FileViewer from "react-file-viewer";
 import "./index.less";
 import Gohome from "../components/goHome";
-import { useEffect } from "react";
 const Privacy = () => {
   return (
     <div className="pdf">
       <Gohome />
       <iframe
+        src="/privacy.html"
         style={{ width: "100%", height: "100%" }}
-        src="https://d3cdeza28ixtol.cloudfront.net/docx/privacy.docx"
       ></iframe>
-      {/* <FileViewer
-        className="pdfBox"
-        fileType="docx"
-        filePath={require("./privacy.docx")}
-      ></FileViewer> */}
     </div>
   );
 };
--- src/pages/pdf/termofuse.jsx
+++ src/pages/pdf/termofuse.jsx
@@ -1,4 +1,3 @@
-import FileViewer from "react-file-viewer";
 import "./index.less";
 import Gohome from "../components/goHome";
 const Termofuse = () => {
@@ -6,14 +5,9 @@ const Termofuse = () => {
     <div className="pdf">
       <Gohome />
       <iframe
+        src="/termofuse.html"
         style={{ width: "100%", height: "100%" }}
-        src="https://d3cdeza28ixtol.cloudfront.net/docx/termofuse.docx"
       ></iframe>
-      {/* <FileViewer
-        className="pdfBox"
-        fileType="docx"
-        filePath={`https://d3cdeza28ixtol.cloudfront.net/docx/termofuse.docx`}
-      ></FileViewer> */}
     </div>
   );
 };
